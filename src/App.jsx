51d99ebdc332ccf799da957972aa57a7a3426baa
Bhuviofficial <bhuvi1928.g@gmail.com>
2025-12-29 12:05:55 +0530

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-import { Routes, Route } from "react-router-dom";
-import DashboardLayout from "./layouts/DashboardLayout";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Nutrition from "./pages/Nutrition";
 import Goals from "./pages/Goals";
 
-<Routes>
-  <Route element={<DashboardLayout />}>
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/nutrition" element={<Nutrition />} />
-    <Route path="/goals" element={<Goals />} />
-  </Route>
-</Routes>;
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" />;
+};
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/nutrition"
+          element={
+            <ProtectedRoute>
+              <Nutrition />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/goals"
+          element={
+            <ProtectedRoute>
+              <Goals />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 export default App;
